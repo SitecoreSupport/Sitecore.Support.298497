e6a3a89bd5bf6543cf2f1a09579f8b0bfab2d776
Anton Kechashin <atk@sitecore.net>
2019-07-12 10:35:46 +0300

    Fix for SXA 1.8.0

---
+++ src/Sitecore.Support.298497/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.298497")]
+[assembly: AssemblyProduct("Sitecore.Support.298497")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.298497/XA/Foundation/Search/Services/FacetService.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.Support.XA.Foundation.Search.Services
+{
+    using Sitecore.Data.Items;
+    using Sitecore.XA.Foundation.Multisite;
+    using Sitecore.XA.Foundation.Search.Services;
+    using Sitecore.XA.Foundation.SitecoreExtensions.Repositories;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using Templates = Sitecore.XA.Foundation.Search.Templates;
+
+    public class FacetService : Sitecore.XA.Foundation.Search.Services.FacetService
+    {
+        public FacetService(ISearchContextService searchContextService, IContentRepository contentRepository, ISharedSitesContext sharedSitesContext) : base(searchContextService, contentRepository, sharedSitesContext)
+        {
+        }
+
+        protected override Item GetMatchingFacetDefinition(IEnumerable<Item> facets, string categoryName)
+        {
+            return facets.FirstOrDefault(i =>
+            {
+                #region Changed code
+                string parameters = i[Templates.Facet.Fields.Parameters].ToLower(CultureInfo.InvariantCulture);
+                categoryName = categoryName.ToLower(CultureInfo.InvariantCulture);
+                return !string.IsNullOrWhiteSpace(parameters) && (categoryName.Contains(parameters) || categoryName.Contains(parameters.Replace(" ", "_"))); 
+                #endregion
+            });
+        }
+    }
+}
